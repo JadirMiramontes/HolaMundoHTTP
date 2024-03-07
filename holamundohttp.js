d6fddb6ed1e78bed87aa154e21fff4ac456885d7
JadirMiramontes <jadirescuela23@gmail.com>
2024-03-07 09:39:53 -0600

    Primer Commit del texto plano

---
+++ holamundohttp.js
@@ -0,0 +1,19 @@
+//llamamos el modulo http
+const http = require('http');
+//establecemos la url o IP de nuestro servidor
+const hostname = '192.168.1.124';
+//Establecemos el puerto de escucha
+const port = 3000;
+//Creamos una instancia http con un reques y un response
+const server = http.createServer((req,res) =>{
+    //el servidor respondera un codigo 200
+    res.statusCode = 200;
+    //El servidor respondera con un texto plano
+    res.setHeader('Content.Type', 'text/plain');
+    //El servidor respondera el mensaje hola mundo
+    res.end('Hola mundo\n');
+});
+
+server.listen(port, hostname,()=>{
+    console.log(`el servidor se esra ejecuntando en http://${hostname}:${port}/`);
+});
